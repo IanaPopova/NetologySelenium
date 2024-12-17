41a1928dd5771f9a0fefc0bb17d0b6d9112a8d71
ynappva@redmachinegames.com <cC255NQxG>
2024-12-17 22:41:23 +0300

    tests added

--- src/test/java/ru/netology/web/DebitCardOrderFormTest.java
+++ src/test/java/ru/netology/web/DebitCardOrderFormTest.java
@@ -5,17 +5,22 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 
-class CallbackTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DebitCardOrderFormTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll() {
+    public static void setupAll() {
         WebDriverManager.chromedriver().setup();
     }
 
@@ -24,7 +29,7 @@ class CallbackTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
+//        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
@@ -36,8 +41,23 @@ class CallbackTest {
     }
 
     @Test
-public void DebitCardOrderFormTest() {
-
+    public void shouldSendFormIfValidDataAndChecked() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        form.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        form.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79061369202");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector(".button__content")).click();
+        WebElement result = driver.findElement(By.cssSelector(".Success_successBlock__2L3Cw"));
+        assertTrue(result.isDisplayed());
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", result.getText().trim());
     }
 
+    @Test
+    public void shouldNotSendFormIfNameInvalid() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        form.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ivanov Ivan");
+        driver.findElement(By.cssSelector(".button__content")).click();
+        WebElement result = driver.findElement(By.cssSelector(".input__sub"));
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", result.getText().trim());
+    }
 }
